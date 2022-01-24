ab99b19743d60260fcbac86c7d79ba7c162b49dc
rrgthompson <rrgthomp@outlook.com>
2022-01-24 16:38:13 +0000

    Cleaned up object

--- twitter-bot/pools.js
+++ twitter-bot/pools.js
@@ -6,34 +6,34 @@ module.exports = pools = [
     include_rts: false,
     tweet_mode: "extended",
   },
-  (params = {
+  {
     screen_name: "Brockwell_Lido",
     count: 10,
     exclude_replies: true,
     include_rts: false,
     tweet_mode: "extended",
-  }),
-  (params = {
+  },
+  {
     screen_name: "WestReservoir",
     count: 10,
     exclude_replies: true,
     include_rts: false,
     tweet_mode: "extended",
-  }),
-  (params = {
+  },
+  {
     screen_name: "coaching_ptp",
     count: 10,
     exclude_replies: true,
     include_rts: false,
     tweet_mode: "extended",
-  }),
-  (params = {
+  },
+  {
     screen_name: "CityCorpHeath",
     count: 10,
     exclude_replies: true,
     include_rts: false,
     tweet_mode: "extended",
-  }),
+  },
 ];
 
 /*
